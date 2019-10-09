69d9a015989acc0ec64eca4b10e3be5fb1021b8d
xc169 <1172330673@qq.com>
2019-10-09 16:10:52 +0800

    Initial commit

---
+++ src/main/java/com/hunau/test/App.java
@@ -0,0 +1,21 @@
+package com.hunau.test;
+
+/**
+ * @param :$params$
+ * @Description:
+ * @Return: $returns$
+ * @开发人员: 杨梦蝶
+ * @单位：湖南农业大学
+ * @Date: $date$ $time$
+ * @开发版本：综合练习Vo.1
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
